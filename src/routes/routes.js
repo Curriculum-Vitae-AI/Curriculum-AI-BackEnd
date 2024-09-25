c5faec95779cc3065815f5f7018c4dfa2b2a9226
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-09-25 00:19:22 -0300

    feat: testa criacao da entidade log

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 
+import LogEntity from '../core/domain/entities/log.js';
+
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
+// TESTANDO CRIAÇÃO DE NOVA ENTIDADE
+router.get('/', async (req, res) => {
+    const logEntity = await LogEntity.create({
+        request: {
+            requestTest: 'request'
+        },
+        response: {
+            responseTest: 'response'
+        },
+        service: 'ROADMAP',
+        date: new Date()
+    });
+    res.send(logEntity);
 });
 
 export default router;
