1e003748929c3c00fba26bdd3bbcd3bc8ca2b4a2
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-09-30 23:03:05 -0300

    test: adiciona teste para log repository

---
+++ src/tests/core/repositories/LogRepositoryTest.js
@@ -0,0 +1,26 @@
+import LogEntity from '../../../core/domain/entities/LogEntity.js';
+import LogRepository from '../../../core/repositories/LogRepository';
+
+jest.mock('../../../core/domain/entities/LogEntity.js');
+jest.mock('../../../core/utils/log/Logger.js', () => ({}));
+
+describe('LogRepository', () => {
+    const logRepository = new LogRepository();
+
+    it('Should return created log entity', async () => {
+        const logEntity = {
+            id: 1,
+            request: {
+                message: 'Test'
+            },
+            response: {
+                message: 'Test'
+            },
+            service: 'ROADMAP',
+            date: new Date()
+        }
+        LogEntity.create.mockResolvedValue(logEntity)
+        const response = await logRepository.save(logEntity);
+        expect(response).toBe(logEntity);
+    })
+})
