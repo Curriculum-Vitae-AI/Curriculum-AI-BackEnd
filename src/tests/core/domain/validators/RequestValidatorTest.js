18e43f65c55efe9afc934df3aad927544774e009
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-10-01 16:38:39 -0300

    test: adiciona test ao validator

--- src/tests/core/domain/validators/RequestValidatorTest.js
+++ src/tests/core/domain/validators/RequestValidatorTest.js
@@ -3,15 +3,30 @@ import RequestValidator from '../../../../core/domain/validators/RequestValidato
 
 describe('validateRoadMapRequest', () => {
     it('Should throw exception when request job does not exists.', () => {
-        const request = {}
-        expect(() => RequestValidator.validateRoadMapRequest(request)).toThrow(ApiException);
-    });
-    it('Should throw exception when request job exists but is empty.', () => {
-        const request = {job: ''}
+        const request = {};
         expect(() => RequestValidator.validateRoadMapRequest(request)).toThrow(ApiException);
     });
     it('Should not throw exception when request job exists and is not empty.', () => {
-        const request = {job: 'Test'}
+        const request = {job: 'Test'};
         expect(() => RequestValidator.validateRoadMapRequest(request)).not.toThrow(ApiException);
+    });
+});
+
+describe('validateMotivationLetterRequest', () => {
+    it('Should throw exception when request company does not exists.', () => {
+        const request = {};
+        expect(() => RequestValidator.validateMotivationLetterRequest(request)).toThrow(ApiException);
+    });
+    it('Should throw exception when request role does not exists.', () => {
+        const request = {company: 'test'};
+        expect(() => RequestValidator.validateMotivationLetterRequest(request)).toThrow(ApiException);
+    });
+    it('Should throw exception when request experience does not exists.', () => {
+        const request = {company: 'teste', role: 'test'};
+        expect(() => RequestValidator.validateMotivationLetterRequest(request)).toThrow(ApiException);
+    });
+    it('Should not throw exception when request is valid.', () => {
+        const request = {company: 'teste', role: 'test', experience: 'test'};
+        expect(() => RequestValidator.validateMotivationLetterRequest(request)).not.toThrow(ApiException);
     });
 });
