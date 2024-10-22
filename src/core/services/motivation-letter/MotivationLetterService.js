907e3fa442fd1e8b09d6813796f6a4c9fcb19c24
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-10-22 00:02:57 -0300

    feat: adiciona gemini a carta de motivacao

--- src/core/services/motivation-letter/MotivationLetterService.js
+++ src/core/services/motivation-letter/MotivationLetterService.js
@@ -1,11 +1,13 @@
 import LogService from '../log/LogService.js';
 import Logger from '../../utils/log/Logger.js';
 import PdfService from '../pdf/PdfService.js';
+import GeminiService from '../gemini/GeminiService.js';
 
 export default class MotivationLetterService {
     constructor() {
         this.logService = new LogService();
         this.pdfService = new PdfService();
+        this.geminiService = new GeminiService();
         this.serviceName = 'CARTA_DE_MOTIVACAO';
     }
     async generateMotivationLetter(request) {
@@ -13,10 +15,12 @@ export default class MotivationLetterService {
         Logger.start(methodName);
         try {
             Logger.info(methodName, 'Gerando carta de motivação...');
-            const response = this.pdfService.generateMotivationLetterPdf();
-            await this.logService.createLog(request, response, this.serviceName);
+            const geminiRequest = this.#defineMotivationLetterTextRequest(request);
+            const geminiResponse = await this.geminiService.getMotivationLetterBody(geminiRequest.data);
+            await this.logService.createLog(geminiRequest, geminiResponse, this.serviceName);
+            const pdf = this.pdfService.generateMotivationLetterPdf(geminiResponse);
             Logger.info(methodName, 'Carta de motivação gerada com sucesso!');
-            return response;
+            return pdf;
         } catch (exception) {
             Logger.error(methodName, exception);
             throw exception;
@@ -24,4 +28,14 @@ export default class MotivationLetterService {
             Logger.finish(methodName);
         }
     }
+    #defineMotivationLetterTextRequest(request) {
+        return {
+            data: `Considere os dados abaixo para montar o corpo de uma carta de motivação: 
+        empresa=${request.company}; vaga=${request.role}; experiencia=${request.experience};
+        para a resposta considere: 
+        code: deve ser 200 para sucesso e 400 para caso a vaga não seja um trabalho de verdade (somente valide se existe sem comparar com a experiencia); 
+        response: nao deve ter quebras de linha nem campos a serem preenchidos posteriomente; 
+        greetings: neste coloque a finalização (ex: atenciosamente, nome)`
+        };
+    };
 }
