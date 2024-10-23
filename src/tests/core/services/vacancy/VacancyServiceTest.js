8f950ce2cd0f0e08b822cfa71b13d0cddb430ad5
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-10-22 23:01:39 -0300

    test: adiciona teste servico vagas

--- src/tests/core/services/vacancy/VacancyServiceTest.js
+++ src/tests/core/services/vacancy/VacancyServiceTest.js
@@ -1,7 +1,7 @@
 import Logger from '../../../../core/utils/log/Logger.js';
 import LogService from '../../../../core/services/log/LogService.js';
 import VacancyService from '../../../../core/services/vacancy/VacancyService.js';
-import { format } from 'date-fns';
+import GeminiService from '../../../../core/services/gemini/GeminiService.js';
 
 jest.mock('../../../../core/utils/log/Logger.js', () => ({
     start: jest.fn(),
@@ -10,14 +10,15 @@ jest.mock('../../../../core/utils/log/Logger.js', () => ({
     error: jest.fn()
 }));
 jest.mock('../../../../core/services/log/LogService.js');
+jest.mock('../../../../core/services/gemini/GeminiService.js');
 
 describe('VacancyService', () => {
     const vacancyService = new VacancyService();
     it('Should requestVacancyLinks from request', async () => {
-        const expected = `Requisição de VAGAS efetuada com sucesso. Log salvo em ${format(new Date(), 'dd/MM/yyyy HH:mm')}`;
-        LogService.prototype.createLog.mockResolvedValue(expected);
+        const expected = {code: 200, response: 'Test'};
+        GeminiService.prototype.getVacancyLinks.mockResolvedValue(expected);
         const response = await vacancyService.requestVacancyLinks('request');
-        expect(response.message).toBe(expected);
+        expect(response).toBe(expected);
         expect(Logger.start).toHaveBeenCalledTimes(1);
         expect(Logger.finish).toHaveBeenCalledTimes(1);
         expect(Logger.info).toHaveBeenCalledTimes(2);
